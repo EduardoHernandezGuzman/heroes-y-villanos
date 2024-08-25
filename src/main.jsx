ab1d0677f5b21dd664f7097228b8650e3b5e6018
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-08-25 20:54:44 +0200

    configuracion de rutas de prueba

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import "./styles/index.css"
+import Pagina1 from "../src/pages/Pagina1.jsx"
+import Pagina2 from "../src/pages/Pagina2.jsx"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/pagina1" element={<Pagina1/>} />
+        <Route path="/pagina2" element={<Pagina2/>} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
-)
+);
